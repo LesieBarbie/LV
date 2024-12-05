ffd47f9ee230e5851b3fa92059879313e2f5a619
Vidziano <bviktora2013@gmail.com>
2024-12-05 22:03:40 +0200

    update

--- Work2/html_form.rb
+++ Work2/html_form.rb
@@ -5,40 +5,48 @@ class HTMLForm
     @fields = []
   end
 
-  def text_field(name:, placeholder: "", label: nil, required: false)
-    label_html = label ? "<label for='#{name}'>#{label}</label>" : ""
-    required_attr = required ? "required" : ""
-    @fields << <<~HTML
-      <div style="margin-bottom: 15px;">
-        #{label_html}
-        <input type='text' id='#{name}' name='#{name}' placeholder='#{placeholder}' #{required_attr} style="padding: 8px; width: 100%; box-sizing: border-box; border: 1px solid #ccc; border-radius: 4px;" />
-      </div>
-    HTML
+  def heading(text:, level: 1)
+    @fields << "<h#{level} style='text-align: center;'>#{text}</h#{level}>"
   end
 
-  def checkbox(name:, label:)
+  def text_field(name:, placeholder: "", label: nil, required: false)
+    add_field_with_label(name, label) do
+      "<input type='text' id='#{name}' name='#{name}' placeholder='#{placeholder}' #{required_attr(required)} style='#{input_style}' />"
+    end
+  end
+
+  def email_field(name:, placeholder: "", label: nil, required: false)
+    add_field_with_label(name, label) do
+      "<input type='email' id='#{name}' name='#{name}' placeholder='#{placeholder}' #{required_attr(required)} style='#{input_style}' />"
+    end
+  end
+
+  def password_field(name:, placeholder: "", label: nil, required: false)
+    add_field_with_label(name, label) do
+      "<input type='password' id='#{name}' name='#{name}' placeholder='#{placeholder}' #{required_attr(required)} style='#{input_style}' />"
+    end
+  end
+
+  def select_field(name:, label: nil, options: [])
+    options_html = options.map { |opt| "<option value='#{opt.downcase.gsub(' ', '_')}'>#{opt}</option>" }.join
+    add_field_with_label(name, label) do
+      "<select id='#{name}' name='#{name}' style='#{input_style}'>#{options_html}</select>"
+    end
+  end
+
+  def radio_group(name:, label:, options: {})
+    options_html = options.map do |value, label_text|
+      "<label><input type='radio' name='#{name}' value='#{value}' style='margin-right: 5px;' /> #{label_text}</label>"
+    end.join("<br>")
+    add_field_with_label(name, label) { options_html }
+  end
+
+  def checkbox(name:, label:, required: false)
     @fields << <<~HTML
       <div style="margin-bottom: 15px;">
         <label>
-          <input type='checkbox' name='#{name}' style="margin-right: 5px;" /> #{label}
+          <input type='checkbox' id='#{name}' name='#{name}' #{required_attr(required)} style="margin-right: 5px;" /> #{label}
         </label>
       </div>
     HTML
   end
-
-  def button(text:, type: "submit", style: "")
-    @fields << <<~HTML
-      <div>
-        <button type='#{type}' style='#{style}'>#{text}</button>
-      </div>
-    HTML
-  end
-
-  def to_html
-    <<~HTML
-      <form action="#{@action}" method="#{@method}" style="max-width: 400px; margin: 0 auto; padding: 20px; border: 1px solid #ccc; border-radius: 5px; background-color: #f9f9f9;">
-        #{@fields.join("\n")}
-      </form>
-    HTML
-  end
-end
